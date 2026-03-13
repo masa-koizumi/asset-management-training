0c8bbe1575b0fd254ff3b64010ebe966019332d5
masa-koizumi <m-koizumi@ems-n.jp>
2026-03-13 10:39:38 +0900

    Add package declaration and imports in AssetController
    Added missing package declaration and imports for AssetController.

--- backend/src/main/java/com/training/asset/controller/AssetController.java
+++ backend/src/main/java/com/training/asset/controller/AssetController.java
@@ -1,3 +1,13 @@
+package com.training.asset.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.training.asset.entity.Asset;
+import com.training.asset.repository.AssetRepository;
+
 @RestController
 @RequestMapping("/assets")
 public class AssetController {
