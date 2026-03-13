8f39cc508b557a45f5a551e80fa08b9d4a4f8a8c
masa-koizumi <m-koizumi@ems-n.jp>
2026-03-13 10:39:03 +0900

    Add AssetRepository interface for asset management

--- backend/src/main/java/com/training/asset/repository/AssetRepository.java
+++ backend/src/main/java/com/training/asset/repository/AssetRepository.java
@@ -1,3 +1,8 @@
+package com.training.asset.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.training.asset.entity.Asset;
+
 public interface AssetRepository
         extends JpaRepository<Asset, Long> {
 }
